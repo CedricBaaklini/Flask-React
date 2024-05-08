74cb4069d533c37728fbadb545ee4cbe53d40fc1
Cedric Baaklini <cabaaklini@icloud.com>
2024-05-08 18:06:00 -0400

    Creating frontend

--- frontend/src/ContactForm.jsx
+++ frontend/src/ContactForm.jsx
@@ -1,11 +1,13 @@
 import {useState} from "react"
 
-const ContactForm = ({}) => {
-	const [firstName, setFirstName] = useState("")
-	const [lastName, setLastName] = useState("")
-	const [email, setEmail] = useState("")
+const ContactForm = ({existingContact = {}, updateCallback}) => {
+	const [firstName, setFirstName] = useState(existingContact.firstName || "");
+	const [lastName, setLastName] = useState(existingContact.lastName || "");
+	const [email, setEmail] = useState(existingContact.email || "");
 
-	const onSubmit = (e) => {
+	const updating = Object.entries(existingContact).length !== 0;
+
+	const onSubmit = async (e) => {
 		e.preventDefault()
 
 		const data = {
@@ -14,45 +16,52 @@ const ContactForm = ({}) => {
 			email
 		}
 
-		const url = "http://127.0.0.1:5000/create_contact"
+		const url = "http://127.0.0.1:5000/" + (updating ? `update_contact/${existingContact.id}` : "create_contact");
 		const options = {
-			method: "POST",
+			method: updating ? "PATCH" : "POST",
 			headers: {
 				"Content-Type": "application/json"
 			},
 			body: JSON.stringify(data)
 		}
+		const response = await fetch(url, options)
+		if (response.status !== 201 && response.status !== 200) {
+			const data = await response.json()
+			alert(data.message)
+		} else {
+			updateCallback();
+		}
 	}
 
-	return <form onSubmit={onSubmit}>
+	return <form onSubmit = {onSubmit}>
 		<div>
-			<label htmlFor="firstName">First Name:</label>
+			<label htmlFor = "firstName">First Name:</label>
 			<input
-				type="text"
-				id="firstName"
-				value={firstName}
-				onChange={(e) => setFirstName(e.target.value)}
+				type = "text"
+				id = "firstName"
+				value = {firstName}
+				onChange = {(e) => setFirstName(e.target.value)}
 			/>
 		</div>
 		<div>
-			<label htmlFor="lastName">Last Name:</label>
+			<label htmlFor = "lastName">Last Name:</label>
 			<input
-				type="text"
-				id="lastName"
-				value={lastName}
-				onChange={(e) => setLastName(e.target.value)}
+				type = "text"
+				id = "lastName"
+				value = {lastName}
+				onChange = {(e) => setLastName(e.target.value)}
 			/>
 		</div>
 		<div>
-			<label htmlFor="email">Email:</label>
+			<label htmlFor = "email">Email:</label>
 			<input
-				type="text"
-				id="email"
-				value={email}
-				onChange={(e) => setEmail(e.target.value)}
+				type = "text"
+				id = "email"
+				value = {email}
+				onChange = {(e) => setEmail(e.target.value)}
 			/>
 		</div>
-		<button type="submit">Create Contact</button>
+		<button type = "submit">{updating ? "Update" : "Create"}</button>
 	</form>
 };
 
